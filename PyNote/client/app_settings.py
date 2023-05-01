1e7f1346c1b51ee28f9c54ac3906096aaae81295
MaxBro <activer12@mail.ru>
2023-05-01 21:57:41 +0300

    AS_settings

--- PyNote/client/app_settings.py
+++ PyNote/client/app_settings.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import (
     QWidget,
+    QDialog,
 
     QLabel,
     QComboBox,
@@ -20,7 +21,7 @@ from settings import (
 from language import lang
 
 
-class App_Settings(QWidget):
+class App_Settings(QDialog):
     update_con = Signal()
 
     def __init__(self, config: Config) -> None:
@@ -33,6 +34,19 @@ class App_Settings(QWidget):
         self.l_main.setSpacing(0)
         self.setFixedSize(QSize(300, 500))
 
+        self.as_lang = AS_lang()
+        self.l_main.addLayout(self.as_lang)
+        self.as_theme = AS_theme()
+        self.l_main.addLayout(self.as_theme)
+        self.as_opacity = AS_opacity()
+        self.l_main.addLayout(self.as_opacity)
+        self.as_name = AS_username()
+        self.l_main.addLayout(self.as_name)
+        self.as_pass = AS_password()
+        self.l_main.addLayout(self.as_pass)
+        self.as_key = AS_waytokey()
+        self.l_main.addLayout(self.as_key)
+
     def update_theme(self):
         pass
 
@@ -41,66 +55,66 @@ class App_Settings(QWidget):
 
 
 class AS_lang(QHBoxLayout):
-    def __init__(self, text: str) -> None:
+    def __init__(self) -> None:
         super().__init__()
         self.setContentsMargins(0, 0, 0, 0)
         self.setSpacing(0)
-        self.text = QLabel(text)
+        self.text = QLabel()
         self.box = QComboBox()
         self.addWidget(self.text)
         self.addWidget(self.box)
 
 
 class AS_theme(QHBoxLayout):
-    def __init__(self, text: str) -> None:
+    def __init__(self) -> None:
         super().__init__()
         self.setContentsMargins(0, 0, 0, 0)
         self.setSpacing(0)
-        self.text = QLabel(text)
+        self.text = QLabel()
         self.box = QComboBox()
         self.addWidget(self.text)
         self.addWidget(self.box)
 
 
 class AS_opacity(QHBoxLayout):
-    def __init__(self, text: str) -> None:
+    def __init__(self) -> None:
         super().__init__()
         self.setContentsMargins(0, 0, 0, 0)
         self.setSpacing(0)
         self.text = QLabel()
-        self.box = QLineEdit(text)
+        self.box = QLineEdit()
         self.addWidget(self.text)
         self.addWidget(self.box)
 
 
 class AS_username(QHBoxLayout):
-    def __init__(self, text: str) -> None:
+    def __init__(self) -> None:
         super().__init__()
         self.setContentsMargins(0, 0, 0, 0)
         self.setSpacing(0)
         self.text = QLabel()
-        self.box = QLineEdit(text)
+        self.box = QLineEdit()
         self.addWidget(self.text)
         self.addWidget(self.box)
 
 
 class AS_password(QHBoxLayout):
-    def __init__(self, text: str) -> None:
+    def __init__(self) -> None:
         super().__init__()
         self.setContentsMargins(0, 0, 0, 0)
         self.setSpacing(0)
         self.text = QLabel()
-        self.box = QLineEdit(text)
+        self.box = QLineEdit()
         self.addWidget(self.text)
         self.addWidget(self.box)
 
 
 class AS_waytokey(QHBoxLayout):
-    def __init__(self, text: str) -> None:
+    def __init__(self) -> None:
         super().__init__()
         self.setContentsMargins(0, 0, 0, 0)
         self.setSpacing(0)
         self.text = QLabel()
-        self.box = QLineEdit(text)
+        self.box = QLineEdit()
         self.addWidget(self.text)
         self.addWidget(self.box)
