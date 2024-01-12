f81884b614e570c755fecd8bbc7722382858992c
MaxBro <activer12@mail.ru>
2024-01-12 23:00:49 +0300

    fix: dock fixes and lang

--- PyNote/lang.py
+++ PyNote/lang.py
@@ -100,21 +100,75 @@ lang = {
         'dia_user_was_deleted': 'Пользователь {username} удален',
     },
     'eng': {
-        'emp_title': 'Название заметки',
-        'emp_inner': 'Текст...',
+        'emp_title': 'Note title',
+        'emp_inner':'Text...',
 
-        'add_but': 'Добавить новую заметку',
-        'settings_but': 'Настройки',
+        'add_but': 'Add a new note',
+        'settings_but': 'Settings',
 
-        'sync_but': 'Синхронизировать заметку',
-        'delete_but': 'Удалить',
+        'sync_but': 'Sync note',
+        'delete_but': 'Delete',
 
-        'as_lang': '',
-        'as_theme': '',
-        'as_opacity': '',
-        'as_text_size': '',
+        'as_lang': 'Language',
+        'as_theme': 'Theme',
+        'as_opacity': 'Transparency',
+        'as_text_size': 'Font size',
 
-        'as_username': '',
-        'as_password': '',
+        'as_username': 'Login',
+        'as_password': 'Password',
+
+        'set_gen': 'Basic',
+        'set_user': 'User',
+        'set_server': 'Network',
+        
+        'set_wid_l': 'Width',
+        'set_wid_i': '',
+
+        'set_hei_l': 'Height',
+        'set_hei_i': '',
+
+        'set_opa_l': 'Transparency',
+        'set_opa_i': '',
+
+        'set_lang_l': 'Language',
+        'set_lang_i': '',
+
+        'set_theme_l': 'Theme',
+        'set_theme_i': '',
+
+        'set_title_l': 'Title Size',
+        'set_title_i': '',
+
+        'set_text_l': 'Text Size',
+        'set_text_i': '',
+
+        'set_nl_l': 'Short bookmark size',
+        'set_nl_i': '',
+
+        'set_username_l': 'User name',
+        'set_username_i': '',
+
+        'set_password_l': 'Password',
+        'set_password_i': '',
+
+        'new_user_b': 'Create an account',
+        'delete_user_b': '! DELETE ACCOUNT!',
+
+        'set_wtkey_l': 'Path to the key',
+        'set_wtkey_i': '',
+
+        'set_host_l': 'Server host',
+        'set_host_i': '',
+
+        'serv_st_l': 'Server status',
+        'serv_st_i': '',
+
+        'set_token_l': 'Access token',
+        'set_token_i': '',
+
+        'dia_new_user_exists': 'This user already exists',
+        'dia_new_user_created': 'User {username} has been created',
+        'dia_delete_user_acces': 'Are you sure you want to delete the user?',
+        'dia_user_was_deleted': 'User {username} has been deleted',
     }
 }
